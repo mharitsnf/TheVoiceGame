e16d31a86025b26b9eb458fec0bd5deb9c85b770
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2020-07-11 10:59:57 +0700

    Add turn queue and base battler class

---
+++ src/turn_queue.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name TurnQueue
+
+var active_character
+
+func initialize():
+	active_character = get_child(0)
+
+func play_turn(target, action):
+	yield(active_character.play_turn(target, action), 'completed')
+	var new_index : int = (active_character.get_index() + 1) % get_child_count()
+	active_character = get_child(new_index)
