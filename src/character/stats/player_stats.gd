0aeeb4de30d302ef941ef064a3401ca9eeea56e0
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2020-07-12 22:08:02 +0700

    Improve shop

--- src/character/stats/player_stats.gd
+++ src/character/stats/player_stats.gd
@@ -2,6 +2,7 @@ extends CharacterStats
 class_name PlayerStats
 
 export var affinity := 0.4 setget set_affinity
+export var potion_count := 1
 
 func set_affinity(value):
 	affinity = value
@@ -10,3 +11,4 @@ func eat_food(amount):
 	current_health += amount
 	if current_health > max_health:
 		max_health = current_health
+		
---
+++ src/game state/game_state.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name GameState
+
+export var enemies_won := 0
+export var first_shop_done := false
